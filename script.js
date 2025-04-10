456e19637142f1563f95245ef012e10dadce2275
JoaoMartinez10 <joaomartinez10@yahoo.com>
2025-04-10 13:41:13 -0400

    v1.3
    Home, lessons. and sponsorship pages are styled for for mobile compatibility. Remade the repo so i don't have an earlier version.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! We will get back to you soon.');
+});
+
+document.getElementById('sponsorshipForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your sponsorship interest! We will contact you shortly.');
+});
